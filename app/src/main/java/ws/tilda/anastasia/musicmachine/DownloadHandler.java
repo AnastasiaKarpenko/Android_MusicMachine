ffdfcc0273fd78ad9c0e16da59be1cbeb0fb8bb5
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-27 17:57:21 +0200

    Handling being killed, restarting the service

--- app/src/main/java/ws/tilda/anastasia/musicmachine/DownloadHandler.java
+++ app/src/main/java/ws/tilda/anastasia/musicmachine/DownloadHandler.java
@@ -7,9 +7,12 @@ import android.util.Log;
 
 public class DownloadHandler extends Handler {
     public static final String TAG = DownloadHandler.class.getSimpleName();
+    private DownloadService mService;
+
     @Override
     public void handleMessage(Message msg) {
         downloadSong(msg.obj.toString());
+        mService.stopSelf(msg.arg1);
     }
 
     //Simulating download process
@@ -24,5 +27,9 @@ public class DownloadHandler extends Handler {
         }
         Log.d(TAG, song + "Song downloaded!");
 
+    }
+
+    public void setService(DownloadService service) {
+        mService = service;
     }
 }
--- app/src/main/java/ws/tilda/anastasia/musicmachine/DownloadService.java
+++ app/src/main/java/ws/tilda/anastasia/musicmachine/DownloadService.java
@@ -3,32 +3,36 @@ package ws.tilda.anastasia.musicmachine;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 
 public class DownloadService extends Service {
     public static final String TAG = DownloadService.class.getSimpleName();
+    private DownloadHandler mHandler;
 
+    @Override
+    public void onCreate() {
+        DownloadThread thread = new DownloadThread();
+        thread.setName("DownloadThread");
+        thread.start();
+
+        while(thread.mHandler == null) {
+
+        }
+        mHandler = thread.mHandler;
+        mHandler.setService(this);
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         String song = intent.getStringExtra(MainActivity.KEY_SONG);
-        downloadSong(song);
+        Message message = Message.obtain();
+        message.obj = song;
+        message.arg1 = startId;
+        mHandler.sendMessage(message);
         return Service.START_REDELIVER_INTENT;
-    }
-
-    private void downloadSong(String song) {
-        long endTime = System.currentTimeMillis() + 10 * 1000;
-        while (System.currentTimeMillis() < endTime) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        Log.d(TAG, song + "Song downloaded!");
-
     }
 
     @Nullable
